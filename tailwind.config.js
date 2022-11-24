da74c1209fe860454cfecdc263a99d31a1845e4c
XudoyberdiyevDilshod <1998xudoyberdiev@gmail.com>
2022-11-24 20:40:05 +0500

    Project ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'header': "url('./images/bgimg.webp')",
+        'hero': "url('./images/hero-bgi.webp')",
+      },
+      backgroundSize: {
+        'imgw': "1440px 650px",
+        'imgh': "731px 500px"
+      },
+      container: {
+        'page': "1110px"
+      },
+      spacing: {
+        px: '1px',
+        98: '24.5rem',
+        100: '25rem',
+        136: '34rem',
+      },
+      fontFamily:{
+        body: ['Josefin Sans']
+      }
+    },
   },
   plugins: [],
 }
